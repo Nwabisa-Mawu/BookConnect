b5f89989a44bd7d851085d5412a3a2aaeaf25cad
NwabisaMawu <nwabisa.mawu@gmail.com>
2023-09-05 21:27:01 +0200

    added meta folder, created Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+const Header = () => {
+
+  return (
+    <>
+      <p>hello</p>
+    </>
+  )
+};
+
+export default Header;
